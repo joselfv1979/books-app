f439e9dadb6e4dbe948d3e87711c8ef24265a133
jose <jose@gmail.com>
2022-03-05 07:43:51 +0100

    menu style improved

--- app/src/components/Menu.tsx
+++ app/src/components/Menu.tsx
@@ -2,31 +2,19 @@ import React from "react";
 import { useUserContext } from "../context/UserContext";
 import { Link, useNavigate } from "react-router-dom";
 import { BoxArrowRight } from "react-bootstrap-icons";
-import "../scss/menu.module.scss";
+import styles from "../scss/menu.module.scss";
 
 const Menu = () => {
   const { user, setUser, isAdmin } = useUserContext();
 
-  const storedUser = localStorage.getItem("user"); 
-
-  // const user = storedUser ? JSON.parse(storedUser) : null
-  // console.log('AdminRoute', {user});
-
-  // const isAdmin = user?.role === 'admin' ? true : false;
-
   const navigate = useNavigate();
-
-  console.log('menu', user);
-
-  console.log('menu_admin', isAdmin);
-  
 
   const logout = () => {
     setUser(null);
     localStorage.removeItem("user");
     navigate("/login");
   };
-  
+
   return (
     <ul className="text-white bg-dark">
       <li>
@@ -43,21 +31,16 @@ const Menu = () => {
           <Link to="/newBook">New Book</Link>
         </li>
       )}
-      <li style={{ marginLeft: "auto" }}>
-        {user ? (
-          <div style={{ display: "flex" }}>
-            <p style={{ marginRight: "1rem" }}>{user.username}</p>
-            <BoxArrowRight
-              color="red"
-              style={{ cursor: "pointer" }}
-              size={26}
-              onClick={logout}
-            />
-          </div>
-        ) : (
+      {user ? (
+        <li className={styles.userSubmenu}>
+          <p>{user.username}</p>
+          <BoxArrowRight className={styles.logoutIcon} onClick={logout} />
+        </li>
+      ) : (
+        <li className={styles.userSubmenu}>
           <Link to="/login">Login</Link>
-        )}
-      </li>
+        </li>
+      )}
     </ul>
   );
 };
